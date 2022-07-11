55af8792898cf154e601a2096e58d10a7b038d1f
GriffinSar <caladoniasaz@gmail.com>
2022-07-11 10:27:26 +0000

    Add constants, score, max questions and inncorect timer penalty these will not change

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,3 +3,9 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
 const progressBarFull = document.getElementById("progressBarFull");
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 14;
+const INNCORECT_TIMER = -10;
+
